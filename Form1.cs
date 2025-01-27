2de67e48b66f48d609fd16a7e1f457732dd90593
prasanna <prasannalakshmik15@gmail.com>
2025-01-27 16:28:06 +0530

    FirstGUIProject

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+namespace FirstGUIProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            MessageBox.Show("You entered : " + text);
+        }
+    }
+}
